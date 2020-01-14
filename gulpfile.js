3adee8719fb5630d00fc98823d035bfceec5c962
nicbovee <nic@nicbovee.com>
2020-01-14 14:28:51 -0700

    changed photo name

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task("fonts", function() {
 
 gulp.task("css", function() {
   return gulp
-    .src(["css/normalize.css", "css/webflow.css", "css/redi-ea1131.webflow.css"])
+    .src(["css/normalize.css", "css/webflow.css", "css/highcountrylandservices-com.webflow.css"])
     .pipe(concat("style.min.css"))
     .pipe(cleanCSS())
     .pipe(gulp.dest("dist/css"));
@@ -54,7 +54,7 @@ gulp.task("html", ["javascript", "css", "fonts", "images"], function() {
         ""
       )
     )
-    .pipe(replace("css/redi-ea1131.webflow.css", "css/style.min.css"))
+    .pipe(replace("css/highcountrylandservices-com.webflow.css", "css/style.min.css"))
     .pipe(
       replace(
         '<script src="js/modernizr.js',
