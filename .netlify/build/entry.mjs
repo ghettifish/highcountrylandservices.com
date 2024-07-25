8c24928881d168d03cbe17ff19257469821ebb91
Nic Bovee <nic@nicbovee.com>
2024-07-25 12:57:58 -0600

    remove the unused cf stuff

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,33 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_BP4iPvqW.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/404.astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "df937689-3ea3-420f-b812-39e739ff1eef"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
