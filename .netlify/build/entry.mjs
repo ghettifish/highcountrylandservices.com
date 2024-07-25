3385062ac9b6f13ce9c5bfdaaf772bfa6298a1be
Nic Bovee <nic@nicbovee.com>
2024-07-25 12:11:11 -0600

    fix errors and warnings

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,33 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_BxwiWdfq.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/404.astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "991b8e41-ca4a-4cb3-98fe-28d5e765aa54"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
